aebf497219b12beef471b63e99891acec61e7bd1
allenhmtang <allenhmtang@tencent.com>
2026-05-18 14:38:39 +0800

    Initial Commit

---
+++ vocalclip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  vocalclip
+//
+//  Created by haomiaotang on 2026/5/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ vocalclip/vocalclipApp.swift
@@ -0,0 +1,17 @@
+//
+//  vocalclipApp.swift
+//  vocalclip
+//
+//  Created by haomiaotang on 2026/5/18.
+//
+
+import SwiftUI
+
+@main
+struct vocalclipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
